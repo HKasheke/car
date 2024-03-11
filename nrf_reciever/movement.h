6bc47ae4412ca007db9a53abc01c981dd09ad536
HKasheke <144255632+HKasheke@users.noreply.github.com>
2024-03-10 23:48:23 -0300

    Update movement.h
    Creating header only file

--- nrf_reciever/movement.h
+++ nrf_reciever/movement.h
@@ -1,3 +1,6 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
 #define STAY  0
 #define FORWARD 1
 #define BACKWARD 2
@@ -11,6 +14,7 @@ const int clockPin = 4;
 ////Pin connected to DS of 74HC595 Pin 14
 const int dataPin = 5;
 
+//Used for double H-Bridge
 const byte direct[5] = {
   B00001010,  //Default/Stay
   B00001010,  //Forward
@@ -18,75 +22,34 @@ const byte direct[5] = {
   B00100110,  //Left
   B00001001}; //Right
 
+void movement ();
+void parseData();
+
 class Movement{
 public:
-    void Dependencies(int latchPin, int clockPin, int dataPin)
-    {
-      pinMode(latchPin, OUTPUT);
-      pinMode(clockPin, OUTPUT);
-      pinMode(dataPin, OUTPUT);
-    }
-  
-    virtual void Go() 
-    {
-        //ground latchPin and hold low for as long as you are transmitting
-        digitalWrite(latchPin, 0);
-        shiftOut(dataPin, clockPin, LSBFIRST, direct[STAY]);
-        //return the latch pin high to signal chip that it
-        //no longer needs to listen for information
-        digitalWrite(latchPin, 1);
-    }
+    void Dependencies(int latchPin, int clockPin, int dataPin);
+    virtual void Go();
 };
 
 class Forward: public Movement {
 public:
-    void Go()
-    {
-        //ground latchPin and hold low for as long as you are transmitting
-        digitalWrite(latchPin, 0);
-        shiftOut(dataPin, clockPin, LSBFIRST, direct[FORWARD]);
-        //return the latch pin high to signal chip that it
-        //no longer needs to listen for information
-        digitalWrite(latchPin, 1);
-    }
+    void Go();
 };
 
 class Backward: public Movement {
 public:    
-    void Go() 
-    {
-        //ground latchPin and hold low for as long as you are transmitting
-        digitalWrite(latchPin, 0);
-        shiftOut(dataPin, clockPin, LSBFIRST, direct[BACKWARD]);
-        //return the latch pin high to signal chip that it
-        //no longer needs to listen for information
-        digitalWrite(latchPin, 1);
-    }
+    void Go();
 };
 
 class Right: public Movement {
 public:
-    void Go() 
-    {
-        //ground latchPin and hold low for as long as you are transmitting
-        digitalWrite(latchPin, 0);
-        shiftOut(dataPin, clockPin, LSBFIRST, direct[RIGHT]);
-        //return the latch pin high to signal chip that it
-        //no longer needs to listen for information
-        digitalWrite(latchPin, 1);
-    }
+    void Go();
 };
 
 class Left: public Movement {
 public:
-    void Go() 
-    {
-        //Shift Register code two change the Hbridge for dynamic movement
-        //ground latchPin and hold low for as long as you are transmitting
-        digitalWrite(latchPin, 0);
-        shiftOut(dataPin, clockPin, LSBFIRST, direct[LEFT]);
-        //return the latch pin high to signal chip that it
-        //no longer needs to listen for information
-        digitalWrite(latchPin, 1);
-    }
+    void Go();
 };
+
+
+#endif
